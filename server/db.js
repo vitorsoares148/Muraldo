7be582a048339fb61bae7c63b0ec296560c1736e
Vitor <vitorgabriel.soares148@gmail.com>
2026-09-16 16:55:30 -0300

    add DB_PORT env variable

--- server/db.js
+++ server/db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2");
 
 const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
 const pool = mysql.createPool({
   host: DB_HOST,
+  port: DB_PORT,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
